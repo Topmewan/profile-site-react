92c176a59b26352fcc63cb88bfc62fbb72300b60
Topmewan <anon8733@gmail.com>
2021-10-10 18:35:38 +0300

    leftside of about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,29 @@
-
+import './About.css';
 
 function About() {
     return (
         <div className='about'>
-            About
+            <div className="about-left">
+                <div className="about-card bg">
+                </div>
+                <div className="about-card">
+                    <img
+                        src="https://images.pexels.com/photos/4841641/pexels-photo-4841641.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                        alt=""
+                        className="about-img"/>
+
+
+                </div>
+            </div>
+            <div className="about-right">
+                <h1 className="about-title">About Me</h1>
+                <p className="about-sub">
+
+                </p>
+                <p className="about-desc">
+
+                </p>
+            </div>
         </div>
     );
 }
